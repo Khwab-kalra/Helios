1bc1651de905a526a8e7fe1522a69d079f76b3ec
Khwab-kalra <58007063+Khwab-kalra@users.noreply.github.com>
2021-02-15 13:03:22 +0530

    Update inductance.m

--- transmissionLineParameters/matlab/inductance.m
+++ transmissionLineParameters/matlab/inductance.m
@@ -1,37 +1,37 @@
 % for calculation of inductace of in transmission line per unit length
 
 disp('########## WELCOME ##########')
-disp('enter the radius of conductor (m^2) ');
+disp('enter the radius of conductor (m^2) ')
 % radius of cross-section area of conductor
-r = input('>> ');
+r = input('>> ')
 
 % conditions
 disp('########## Choose an option ##########')
 disp('1. For single conductor')
 disp('2. For three phase transmission line ')
 
-opt1 = input('>> ');
+opt1 = input('>> ')
 
 if opt1 == 1
     disp('Enter the value of distance from the wire')
-    d = input('>> ');
+    d = input('>> ')
     calcInductance(d,r)
 else
     disp('########## Choose the option ##########')
     disp('1. For symmetrical arrangement')
     disp('2. For Transposed arrangment')
     
-    opt2 = input('>> ');
+    opt2 = input('>> ')
     
     if opt2 == 2
-        Dab = input('Enter the distance between conductor A and conductor B');
-        Dbc = input('Enter the distance between conductor B and conductor C');
-        Dca = input('Enter the distance between conductor C and conductor A');
+        Dab = input('Enter the distance between conductor A and conductor B')
+        Dbc = input('Enter the distance between conductor B and conductor C')
+        Dca = input('Enter the distance between conductor C and conductor A')
         % geometric mean distance
-        GMD = (Dab+Dbc+Dca)^(1/3);
+        GMD = (Dab+Dbc+Dca)^(1/3)
         calcInductance(GMD,r)
     else
-        D = input('Enter the distance between conductors');
+        D = input('Enter the distance between conductors')
         calcInductance(D,r)
     end
 end
@@ -39,17 +39,17 @@ end
 % for calculation of inductance
 function l = calcInductance(d,r)
     % magnetic permebility in non-magnetic material
-    meu = 4*pi*10^(-7);
+    meu = 4*pi*10^(-7)
     % geometric mean radius
-    GMR = 0.7788*r;
+    GMR = 0.7788*r
     % inductance
-    l = (meu/(2*pi))*log(d/GMR);
+    l = (meu/(2*pi))*log(d/GMR)
     disp(l)
     % graph
     disp('If you want to plot the graph, then enter the max-limit of distance')
-    dMax = input('>> ');
+    dMax = input('>> ')
     varD = d:0.1:dMax;
-    varL = (meu/(2*pi))*log(varD/GMR);
+    varL = (meu/(2*pi))*log(varD/GMR)
     plotGraph(varD,varL)
 end
 
@@ -60,5 +60,5 @@ function msg = plotGraph(x,y)
     title('Inductance of transmission line')
     xlabel('distance (m)')
     ylabel('inductance (H/m)')
-    msg = 'ploting graph...';
+    msg = 'ploting graph...'
 end
